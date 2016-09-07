c68ff9c0b5bc3501a3247ad0e0e773e90efb1f12
AStooke <adam.stooke@gmail.com>
2016-09-07 14:34:21 -0700

    doc rev

--- docs/source/examples/index.py
+++ docs/source/examples/index.py
@@ -9,10 +9,8 @@ def some_function():
     time.sleep(2)
     gt.stamp('another_function')
 
-gt.reset_root()
 some_function()
 gt.stamp('some_function')
 time.sleep(1)
 gt.stamp('another_method')
-gt.stop()
 print gt.report()
